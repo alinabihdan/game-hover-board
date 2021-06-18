33ce62008eeec05262fe4034447eb3762a403486
alinabihdan <alina.bigdan1@gmail.com>
2021-06-18 11:06:27 +0300

    done

---
+++ js/index.js
@@ -0,0 +1,43 @@
+const board = document.querySelector("#board");
+const SQUARES_NUMBER = 600;
+const colors = [
+  "#dda0dd",
+  "#da70d6",
+  "#ff00ff",
+  "#c71585",
+  "#ba55d3",
+  "#9370db",
+  "#9400d3",
+  "#8b008b",
+];
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", () => {
+    setColor(square);
+  });
+
+  square.addEventListener("mouseleave", () => {
+    removeColor(square);
+  });
+
+  board.append(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = "#1d1d1d";
+  element.style.boxShadow = "0 0 2px #000";
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
